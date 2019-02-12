208f0e1e6aa1ff995143239328ec33fc9ea3a4c5
gothraven <szaghbane@gmail.com>
2019-02-12 11:57:42 +0100

    [FEATURE] prepare for heroku app

--- bot.py
+++ bot.py
@@ -12,28 +12,27 @@ from src.unfollow_protocol import unfollow_protocol
 bot = InstaBot(
     login=os.environ.get('INSTA_USER', ''),
     password=os.environ.get('INSTA_PASSWORD', ''),
-    start_at_h=13,
-    start_at_m=46,
-    end_at_h=17,
-    end_at_m=45,
-    like_per_day=3000,
-    comments_per_day=2000,
-    tag_list=['fuckology', 'fuckologyquotes', 'quotes',
-            'follow', 'morningvibes', 'morning', 'goodmorning',
-            'photography', 'love', 'instagood', 'morningmotivation',
-            'sunrise', 'sky', 'photooftheday', 'happy', 'picoftheday',
-            'goodvibes', 'naturephotography', 'like', 'sun', 'indiea',
-            'lifestyle', 'mornings', 'travel', 'ig', 'tea', 'photo',
-            'positivevibes', 'bhfyp', 'fucklove', 'comments', 'creativity',
-            'bestoftheday', 'follow', 'fun', 'funny', 'cool', 'amazing',
-            'awesome', 'friends', 'quote', 'quotes', 'comment', 'word',
-            'fuckology', 'fuckologyquotes', 'fuckology', 'quotes',
-            'motivation', 'follow', 'inspiration', 'instagood',
-            'quoteoftheday', 'poetry', 'motivationalquotes',
-            'lifequotes', 'inspirationalquotes', 'quotestoliveby'],
+    start_at_h=9,
+    start_at_m=0,
+    end_at_h=20,
+    end_at_m=0,
+    like_per_day=2000,
+    comments_per_day=1000,
+    tag_list=[
+        'fuckology', 'fuckologyquotes', 'quotes', 'follow', 'morningvibes', 'morning', 'goodmorning',
+        'photography', 'love', 'instagood', 'morningmotivation', 'sunrise', 'sky', 'photooftheday', 'happy',
+        'picoftheday', 'goodvibes', 'naturephotography', 'like', 'sun', 'indiea', 'lifestyle', 'mornings',
+        'travel', 'ig', 'tea', 'photo', 'positivevibes', 'bhfyp', 'fucklove', 'comments', 'creativity',
+        'bestoftheday', 'follow', 'fun', 'funny', 'cool', 'amazing', 'awesome', 'friends', 'quote', 'quotes',
+        'comment', 'word', 'fuckology', 'fuckologyquotes', 'fuckology', 'quotes', 'motivation', 'follow',
+        'inspiration', 'instagood', 'quoteoftheday', 'poetry', 'motivationalquotes', 'lifequotes', 'inspirationalquotes',
+        'quotestoliveby', 'parisien', 'parisienne', 'paris', 'igparis', 'igersparis', 'lavieestbelle', 'fitfrenchies',
+        'celibataire', 'impatience', 'parisjetaime', 'parismonamour', 'igfrance', 'bordeaux', 'lyon', 'bleublancrouge',
+        'allezlesbleus', 'allezlesbleues', 'equipedefrance', 'naturel'
+    ],
     tag_blacklist=[],
     user_blacklist={},
-    max_like_for_one_tag=51,
+    max_like_for_one_tag=27,
     follow_per_day=0, #follow_per_day = 500
     follow_time=0, #follow_time=1 * 60 * 60,
     unfollow_per_day=0, #unfollow_per_day=300
@@ -59,64 +58,5 @@ bot = InstaBot(
         'follow', 'follower', 'gain', '.id', '_id', 'bags'
     ],
     unfollow_whitelist=[])
-while True:
 
-    #print("# MODE 0 = ORIGINAL MODE BY LEVPASHA")
-    #print("## MODE 1 = MODIFIED MODE BY KEMONG")
-    #print("### MODE 2 = ORIGINAL MODE + UNFOLLOW WHO DON'T FOLLOW BACK")
-    #print("#### MODE 3 = MODIFIED MODE : UNFOLLOW USERS WHO DON'T FOLLOW YOU BASED ON RECENT FEED")
-    #print("##### MODE 4 = MODIFIED MODE : FOLLOW USERS BASED ON RECENT FEED ONLY")
-    #print("###### MODE 5 = MODIFIED MODE : JUST UNFOLLOW EVERYBODY, EITHER YOUR FOLLOWER OR NOT")
-
-    ################################
-    ##  WARNING   ###
-    ################################
-
-    # DON'T USE MODE 5 FOR A LONG PERIOD. YOU RISK YOUR ACCOUNT FROM GETTING BANNED
-    ## USE MODE 5 IN BURST MODE, USE IT TO UNFOLLOW PEOPLE AS MANY AS YOU WANT IN SHORT TIME PERIOD
-
-    mode = 0
-
-    #print("You choose mode : %i" %(mode))
-    #print("CTRL + C to cancel this operation or wait 30 seconds to start")
-    #time.sleep(30)
-
-    if mode == 0:
-        bot.new_auto_mod()
-
-    elif mode == 1:
-        check_status(bot)
-        while bot.self_following - bot.self_follower > 200:
-            unfollow_protocol(bot)
-            time.sleep(10 * 60)
-            check_status(bot)
-        while bot.self_following - bot.self_follower < 400:
-            while len(bot.user_info_list) < 50:
-                feed_scanner(bot)
-                time.sleep(5 * 60)
-                follow_protocol(bot)
-                time.sleep(10 * 60)
-                check_status(bot)
-
-    elif mode == 2:
-        bot.bot_mode = 1
-        bot.new_auto_mod()
-
-    elif mode == 3:
-        bot.bot_mode = 3
-        while(bot.login_status == 1):
-            bot.unfollow_recent_feed()
-            time.sleep(5)
-
-    elif mode == 4:
-        feed_scanner(bot)
-        time.sleep(60)
-        follow_protocol(bot)
-        time.sleep(10 * 60)
-
-    elif mode == 5:
-        bot.bot_mode = 2
-        unfollow_protocol(bot)
-
-    else:
-        print("Wrong mode!")
+bot.mainloop()
