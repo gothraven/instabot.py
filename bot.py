1652496a2a17bfbcada0f3d1bd03464cdd79e78f
gothraven <szaghbane@gmail.com>
2019-03-13 14:24:31 +0100

    Update bot.py to work with the new changes

--- bot.py
+++ bot.py
@@ -1,13 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import os
-import time
 
-from src import InstaBot
-from src.check_status import check_status
-from src.feed_scanner import feed_scanner
-from src.follow_protocol import follow_protocol
-from src.unfollow_protocol import unfollow_protocol
+import os
+from instabot_py import InstaBot
 
 bot = InstaBot(
     login=os.environ.get('INSTA_USER', ''),
